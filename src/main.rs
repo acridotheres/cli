262dd9c1ad72bf6e9f84440cbb448ccf8dc57fe8
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-07-14 20:13:46 +0200

    chore: added ZIP extraction and metadata

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,50 @@ struct Args {
 
     /// Show archive metadata
     #[arg(short, long)]
-    metadata: Option<String>,
+    metadata: bool,
+
+    /// Input type
+    #[arg(short = 't', long = "type")]
+    input_type: Option<String>,
+
+    /// Path to input file
+    #[arg(short, long)]
+    input: Option<String>,
+
+    /// Path to output file or directory
+    #[arg(short, long)]
+    output: Option<String>,
+
+    /// List files in archive
+    #[arg(short, long)]
+    list: bool,
+
+    /// Extract files from archive
+    #[arg(short = 'x', long = "extract")]
+    extract: bool,
+
+    /// by path
+    #[arg(short = 'p', long = "path", value_name = "PATH")]
+    path: Option<String>,
+
+    /// by index (faster than by path)
+    #[arg(short = '1', long = "index", value_name = "INDEX")]
+    index: Option<u32>,
+
+    /// "all" (combine with -x or similar)
+    #[arg(short, long)]
+    all: bool,
 }
 
 fn get_command(args: &Args) -> &'static str {
     if args.version {
         "version"
-    } else if args.metadata != None {
+    } else if args.metadata {
         "metadata"
+    } else if args.list {
+        "list"
+    } else if args.extract {
+        "extract"
     } else {
         ""
     }
@@ -34,10 +70,27 @@ fn main() {
 
     let command = get_command(&args);
 
+    let format = args.input_type.unwrap_or_else(|| {
+        // add format detection here
+        "".to_string()
+    });
+    let format = format.as_str();
+
     if !args.json {
         match command {
             "version" => text::get_version(),
-            "metadata" => text::zip_metadata(args.metadata.unwrap().as_str()),
+            "metadata" => match format {
+                "zip" => text::zip_metadata(args.input.unwrap().as_str()),
+                _ => println!("Unknown format"),
+            },
+            "list" => match format {
+                "zip" => text::zip_list(args.input.unwrap().as_str()),
+                _ => println!("Unknown format"),
+            },
+            "extract" => match format {
+                "zip" => text::zip_extract(args.input.unwrap().as_str(), args.output.unwrap().as_str(), args.index, args.path, args.all),
+                _ => println!("Unknown format"),
+            },
             _ => println!("Nothing to do, try --help"),
         }
     } else {
